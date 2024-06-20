b0942f678be38c09ddc489a637114d5a1593a516
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2024-06-20 00:43:30 -0500

    actualizacion

---
+++ firebase/database.js
@@ -0,0 +1,4 @@
+export const apiKey = 'AIzaSyB3an13oOJfBt2bjANKplAd01k-lKLywt0'
+export const authUrl = 'https://identitytoolkit.googleapis.com'
+export const baseUrl = 'https://tiendavirtual-app-658e0-default-rtdb.firebaseio.com'
+ 
